f1811a86a3845c7c5f8a455b08a311703d655e7a
Victor Safronovich <vsafronovich@gmail.com>
2012-08-26 15:11:09 +0600

    fix jinja syntax error

--- speedydeploy/templates/django/settings_sphinxsearch.py
+++ speedydeploy/templates/django/settings_sphinxsearch.py
@@ -1,4 +1,4 @@
 SPHINX_API_VERSION = {{sphinxsearch.api_version|default('0x116')}}
-SPHINX_ROOT = "{{sphinxsearch.domain|default('localhost')}}"
+SPHINX_ROOT = "{{sphinxsearch.host|default('localhost')}}"
 SPHINX_HOST = "/home/{{user}}/run/searchd.sock"
 SPHINX_PORT = {{sphinxsearch.port|default('11312')}}
