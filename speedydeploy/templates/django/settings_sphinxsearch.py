5e5e0c204ad9de78a0c755cef4503ac94bd25ccc
Victor Safronovich <vsafronovich@gmail.com>
2012-09-03 19:56:36 +0600

    do not set SPHINX_PORT if no port setted

--- speedydeploy/templates/django/settings_sphinxsearch.py
+++ speedydeploy/templates/django/settings_sphinxsearch.py
@@ -1,4 +1,5 @@
 SPHINX_API_VERSION = {{sphinxsearch.api_version|default('0x116')}}
 SPHINX_ROOT = "/home/{{user}}/data/sphinxsearch/"
 SPHINX_SERVER = "{{sphinxsearch.host|default("/home/%s/run/searchd.sock" % user)}}"
-SPHINX_PORT = {{sphinxsearch.port|default(None)}}
+{% if sphinxsearch.port %}SPHINX_PORT = {{sphinxsearch.port}}{% endif %}
+
