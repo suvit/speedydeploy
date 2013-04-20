556cb23204ad25c9f4f39106d563928ed3dd4f52
Victor Safronovich <vsafronovich@gmail.com>
2013-04-20 13:13:50 +0600

    added hetzner provider

---
+++ speedydeploy/providers/hetzner.py
@@ -0,0 +1,43 @@
+from fabric import api as fab
+
+from ..base import Ubuntu, Ubuntu104
+from ..deployment import _
+from ..project.cron import CronTab
+from ..project import LogRotate
+
+from .base import Provider
+
+class Hetzner(Provider):
+    def __init__(self):
+        super(Linode, self).__init__()
+
+        fab.env.remote_dir = _("/home/%(user)s/")
+        fab.env.home_dir = _("/home/%(user)s/")
+
+        fab.env.cron = CronTab()
+        fab.env.logrotate = LogRotate()
+
+class VQ7(Hetzner):
+    pass
+
+
+class VQ12(Hetzner):
+    pass
+
+
+class EX4(Hetzner):
+    pass
+
+
+class EX4S(EX4):
+    pass
+
+
+class EX5(Hetzner):
+    pass
+
+
+class EX6(Hetzner):
+    pass
+
+
