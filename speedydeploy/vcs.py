0c7d1b5980129e2779865298095f6579619f6569
Victor Safronovich <vsafronovich@gmail.com>
2013-06-25 13:26:15 +0600

    use separate repo name instead of project name

--- speedydeploy/vcs.py
+++ speedydeploy/vcs.py
@@ -11,20 +11,25 @@ from deployment import command
 
 
 class VCS(object):
+
+    def __init__(self):
+        if 'vcs_repo_name' not in fab.env:
+            fab.env['vcs_repo_name'] = fab.env['project_name']
+
     def install_development_libraries(self):
         pass
 
     @command(namespace='vcs', aliases=('deploy',))
     def deploy(self, force_remove=False):
-        if exists(_('%(remote_dir)s/%(project_name)s')) and not force_remove:
-            with fab.cd(_('%(remote_dir)s/%(project_name)s/')):
+        if exists(_('%(remote_dir)s/%(vcs_repo_name)s')) and not force_remove:
+            with fab.cd(_('%(remote_dir)s/%(vcs_repo_name)s/')):
                 self.update()
         else:
             with fab.cd(_('%(remote_dir)s/')):
                 with fab.settings(warn_only=True):
-                    fab.run(_("rm -rf %(project_name)s"))
+                    fab.run(_("rm -rf %(vcs_repo_name)s"))
                     self.clone()
-        with fab.cd(_('%(remote_dir)s/%(project_name)s/')):
+        with fab.cd(_('%(remote_dir)s/%(vcs_repo_name)s/')):
            self.remove_pyc()
 
     def export(self):
@@ -44,7 +49,7 @@ class VCS(object):
 
 class SVN(VCS):
     def find_last_rev(self):
-        out = fab.local(_('svn log -ql1 %(svn_repo)s/%(project_name)s'),
+        out = fab.local(_('svn log -ql1 %(svn_repo)s/%(vcs_repo_name)s'),
                         capture=True)
         return out.split()[1].strip('r')
 
@@ -54,13 +59,13 @@ class SVN(VCS):
     def export(self):
         # TODO svn_rev svn_path move to self attrs
         fab.local("mkdir tmp")
-        fab.local(_("svn export -r %(svn_rev)s %(svn_path)s ./tmp/%(project_name)s"))
+        fab.local(_("svn export -r %(svn_rev)s %(svn_path)s ./tmp/%(vcs_repo_name)s"))
 
     def update(self):
         fab.run('svn up')
 
     def clone(self):
-        fab.run(_('svn checkout -r %(svn_rev)s %(svn_path)s %(project_name)s'))
+        fab.run(_('svn checkout -r %(svn_rev)s %(svn_path)s %(vcs_repo_name)s'))
 
 
 class SVNServer(object):
@@ -76,14 +81,14 @@ class GIT(VCS):
 
     @property
     def use_submodules(self):
-        return exists(_('%(remote_dir)s/%(project_name)s/.gitmodules'))
+        return exists(_('%(remote_dir)s/%(vcs_repo_name)s/.gitmodules'))
 
     def install_development_libraries(self):
         fab.env.os.install_package('git-core')
 
     def export(self):
         fab.local("mkdir tmp")
-        fab.local(_("git clone ./tmp/%(project_name)s"))
+        fab.local(_("git clone ./tmp/%(vcs_repo_name)s"))
 
     def update(self):
         fab.run('git pull')
@@ -93,7 +98,7 @@ class GIT(VCS):
     def clone(self):
         fab.run(_('git clone --recursive %(git_path)s'))
         #if self.use_submodules:
-        #    with fab.cd(_('%(remote_dir)s/%(project_name)s/')):
+        #    with fab.cd(_('%(remote_dir)s/%(vcs_repo_name)s/')):
         #        fab.run('git submodule init')
         #        fab.run('git submodule update')
 
@@ -124,15 +129,15 @@ class NoneVCSWithExport(NoneVCS):
         fab.local("rm -rf tmp/")
 
     def prepare(self):
-        fab.local(_("cd tmp/ && tar -czf %(project_name)s.tgz %(project_name)s"))
-        fab.put(_('tmp/%(project_name)s.tgz'), _('%(remote_dir)s/tmp/'))
+        fab.local(_("cd tmp/ && tar -czf %(vcs_repo_name)s.tgz %(vcs_repo_name)s"))
+        fab.put(_('tmp/%(vcs_repo_name)s.tgz'), _('%(remote_dir)s/tmp/'))
 
         with fab.cd(_('%(remote_dir)s/tmp')):
-            fab.run(_("tar -xzf %(remote_dir)s/tmp/%(project_name)s.tgz"))
+            fab.run(_("tar -xzf %(remote_dir)s/tmp/%(vcs_repo_name)s.tgz"))
 
         with fab.cd(_('%(remote_dir)s')):
             fab.env.os.mkdir("build")
-            fab.run(_("mv %(remote_dir)s/tmp/%(project_name)s build"))
+            fab.run(_("mv %(remote_dir)s/tmp/%(vcs_repo_name)s build"))
 
     def deploy_old(self):
         if not getattr(fab.env, 'debug', False):
@@ -148,8 +153,8 @@ class NoneVCSWithExport(NoneVCS):
         with fab.cd(_('%(remote_dir)s')):
             try:
                 with fab.settings(warn_only=True):
-                    fab.run(_("rm -rf %(project_name)s"))
-                    fab.run(_("mv build/%(project_name)s %(project_name)s"))
+                    fab.run(_("rm -rf %(vcs_repo_name)s"))
+                    fab.run(_("mv build/%(vcs_repo_name)s %(vcs_repo_name)s"))
             finally:
                 fab.run("rm -rf build")
         fab.env.os.set_permissions()
